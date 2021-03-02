f82f92856587affd1faa82390f333633882f5375
Compuestudio Amilcar <78876755+amilcar-compuestudio@users.noreply.github.com>
2021-03-01 20:16:28 -0500

    fix

--- index.php
+++ index.php
@@ -1,12 +1,51 @@
 <!DOCTYPE html>
-<html lang="en">
+<!-- 
+        * Pagina Eductiva
+        * https://midominio.com/
+        * @Desarrollador Amilcar Gómez <amilcar@compuestudio.edu.co>
+        * Fecha 23-02-2021
+        * Development Owned by the Entity Compuestudio
+        * @license
+        * @Released under the MIT license and GPL licenses.
+        * Modificado por
+        * Fecha Modificación 
+        * Comentarios: Mi Primera página 
+ -->
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page HTML - PHP</title>
+    <title>Página Inicial</title>
 </head>
 <body>
-    <h1>Hola Mundo<h1>
+    <header>
+        <h1>Bienvenidos</h1>
+    </header>
+    <nav>
+        <a href="tablas.html">Tablas HTML</a>
+        <a href="parrafos.html">Párrafos</a>
+        <a href="formularios.html">Formularios</a>
+        <a href="images.html">Images</a>
+        <a href="videos-audios.html">Videos y Audios</a>
+        <a href="iframe.html">Iframe</a>
+    </nav>
+    <main>
+        <section>
+            <h1>Titulo Principal</h1>
+            <p><abbr title="Servicio de Inteligencia de EU">CIA</abbr> ipsum dolor sit amet <strong>negrita</strong> adipisicing elit. Quaerat autem praesentium atque quos delectus modi magni architecto <span>SPAN</span>, <b>negrita</b> <i>distinctio.</i> <mark>Quis</mark> quae eius ab <q>maxime</q> cupiditate velit ex expedita atque?</p>
+        </section>
+    </main>
+    <aside>
+        <h3>Páginas Relacionadas</h3>
+        <a href="#">Página</a>
+    </aside>
+    <footer>
+        <address>
+            Amilcar Gómez <br>
+            Bogotá <br>
+            Colombia 
+        </address>
+    </footer>
 </body>
 </html>
